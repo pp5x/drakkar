fb5b077e3bf79c19fdfcb12b59e82cd0d81c8abd
Pierre Pagnoux <pierre.pagnoux@epita.fr>
2016-09-05 17:50:15 +0200

    Bug mavericks: fix EXT4 partition detection & physical volume mapping

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ CURDIR=$(dirname $0)
 NAME=$(basename $0)
 DEBUG=1
 EXT4SIZE=-1
+ROOTDISK=disk0s2
+TARGETDISK=disk0s5
 
 source $CURDIR/err_colors.sh
 
@@ -38,7 +40,7 @@ function usage() {
   echo -e '-h\t\tPrints this message.'
   echo -e '--format size\tCreates a EXT4 partition with {size}. Does nothing if'
   echo -e '             \tnot specified and will skip this step. It might lead'
-  echo -e '             \tto an error if there is no disk0s4 partition. {size} is'
+  echo -e "             \tto an error if there is no $TARGETDISK partition. {size} is"
   echo -e '             \tin GB [ 1; 999 ].'
 }
 
@@ -56,14 +58,14 @@ function optparse() {
           exit 0
         else
           EXT4SIZE=$2
-          info "disk0s4 EXT4 partition will be of size ${EXT4SIZE} GB."
+          info "$TARGETDISK EXT4 partition will be of size $EXT4SIZE GB."
         fi
         ;;
       *)
     esac
   fi
   if [ "$EXT4SIZE" -eq -1 ]; then
-    info 'disk0s4 EXT4 partitionning will be skipped.'
+    info "$TARGETDISK EXT4 partitionning will be skipped."
   fi
 }
 
@@ -74,7 +76,7 @@ optparse $@
 ################################################################################
 
 info 'Check that System Integrity Protection is turned off'
-if ! csrutil status | grep -q 'disabled'; then
+if hash csrutil 2> /dev/null && csrutil status | grep -q 'disabled'; then
   info 'You must disable System Integrity Protection:'
   info 'Reboot on Recovery HD and execute the command: csrutil disable'
   fatal_error 'System Integrity Protection is enabled.'
@@ -99,15 +101,15 @@ trap 'cleanup' ERR
 # Convert from Core Storage to HFS+ if required.
 ################################################################################
 
-if diskutil info disk0s2 | grep -q 'Core Storage'; then
+if diskutil info $ROOTDISK | grep -q 'Core Storage'; then
   info '/dev/disk0s2 is a Core Storage Volume. Converting to HFS+'
-  COREVOLUME=$(diskutil list | grep -A1 'Logical Volume on disks2' | tail -1)
+  COREVOLUME=$(diskutil list | grep -A1 "Logical Volume on $ROOTDISK" | tail -1)
   sudo diskutil coreStorage revert $COREVOLUME
-  success 'disk0s2 has been converted to HFS+.'
+  success "$ROOTDISK has been converted to HFS+."
   info "Please $RED*REBOOT*$NORMAL now and run this script again!"
   exit 2
 else
-  success 'Volume disk0s2 is HFS+.'
+  success "Volume $ROOTDISK is HFS+."
 fi
 
 ################################################################################
@@ -198,36 +200,59 @@ fi
 ################################################################################
 
 if ! [ "$EXT4SIZE" -eq -1 ]; then
-  info 'Check disk0s4 EXT4 filesystem'
-  if diskutil list disk0 | grep -q 'Linux Filesystem'; then
-    if diskutil list disk0 | grep -q 'Linux Filesystem.*disk0s4$'; then
-      info 'disk0s4 EXT4 is already created. Formatting in EXT4...'
-      diskutil eraseVolume UFSD_EXTFS4 'Linux' disk0s4
-      success 'disk0s4 volume has been erased to EXT4 format.'
+  info "Check $TARGETDISK EXT4 filesystem"
+  if diskutil list disk0 | grep -q 'Linux Filesystem\|Microsoft Basic Data'; then
+    if diskutil list disk0 | grep "\(Linux Filesystem\|Microsoft Basic Data\).*${TARGETDISK}$"; then
+      info "$TARGETDISK EXT4 is already created. Formatting in EXT4..."
+      diskutil eraseVolume UFSD_EXTFS4 '1' $TARGETDISK
+      success "$TARGETDISK volume has been erased to EXT4 format."
     else
       debug diskutil list && diskutil list
-      fatal_error 'Linux Filesystem found but not at disk0s4. Please check.'
+      fatal_error "Linux Filesystem found but not at /dev/$TARGETDISK. Please check."
     fi
   else
-    info 'Resizing disk0s2 to create EXT4 disk0s4 partition...'
-    sudo diskutil resizeVolume disk0s2 ${EXT4SIZE}g 1 UFSD_EXTFS4 'Linux' 0g
-    success 'disk0s4 EXT4 volume has been created.'
+    info "Resizing $ROOTDISK to create EXT4 $TARGETDISK partition..."
+    sudo diskutil resizeVolume $ROOTDISK ${EXT4SIZE}g UFSD_EXTFS4 '1' 0g
+    success "$TARGETDISK EXT4 volume has been created."
   fi
 else
   info 'Skipping EXT4 partition creation'
 fi
 
-if diskutil list disk0 | grep -q 'Linux Filesystem.*disk0s4$'; then
-  success 'Found disk0s4 EXT4 volume.'
+if diskutil list disk0 | grep -q "\(Linux Filesystem\|Microsoft Basic Data\).*${TARGETDISK}$"; then
+  success "Found $TARGETDISK EXT4 volume."
 else
-  fatal_error 'A disk0s4 partition is required beyond this point.'
+  fatal_error "A $TARGETDISK partition is required beyond this point."
 fi
 
 ###############################################################################
 # Virtual disk to physical disk mapping
 ###############################################################################
 
-# TODO
+if ! docker-machine status docker-vm | grep -q 'Running'; then
+  VIRTUALDISK=`mktemp /tmp/main.vmdk.XXXXXX` || \
+    fatal_error 'Cannot create temporary virtual disk'
+
+  rm $VIRTUALDISK
+
+  # Physical partition must remain unmounted
+  if [ -d /Volumes/1 ]; then
+    info 'Unmounting the "1" partition'
+    diskutil umount '1'
+  fi
+
+  info 'Creating vmdk'
+  sudo vboxmanage internalcommands createrawvmdk -filename $VIRTUALDISK -rawdisk /dev/${TARGETDISK}
+  sudo chmod 666 $VIRTUALDISK
+  sudo chmod 666 /dev/${TARGETDISK}
+
+  info 'Attaching physical partition to the docker-vm'
+  vboxmanage storageattach docker-vm --storagectl "SATA" --port 2 --device 0 --type hdd --medium $VIRTUALDISK
+
+  info 'Starting docker-vm'
+  docker-machine start docker-vm
+  success 'Docker machine ready'
+fi
 
 info 'Exiting...'
 cleanup
